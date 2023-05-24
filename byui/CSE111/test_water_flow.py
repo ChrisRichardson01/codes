33cd82be99b559002364c20eb74c3714e10fa87a
ChrisRichardson01 <ric20054@byui.edu>
2023-05-24 14:20:50 -0500

    updates

--- byui/CSE111/test_water_flow.py
+++ byui/CSE111/test_water_flow.py
@@ -71,3 +71,105 @@ test_pressure_loss_from_pipe()
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
 pytest.main(["-v", "--tb=line", "-rN", __file__])
+
+from pytest import approx
+import pytest
+from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe, pressure_loss_from_fittings
+
+# Existing test functions...
+
+def test_pressure_loss_from_fittings():
+    test_cases = [
+        (0, 3, 0, 0.001),
+        (1.65, 0, 0, 0.001),
+        (1.65, 2, -0.109, 0.001),
+        (1.75, 2, -0.122, 0.001),
+        (1.75, 5, -0.306, 0.001)
+    ]
+
+    tolerance = 0.001
+
+    for velocity, quantity, expected_loss, approx_tolerance in test_cases:
+        result = pressure_loss_from_fittings(velocity, quantity)
+        diff = abs(result - expected_loss)
+        print(f"Fluid Velocity: {velocity} m/s")
+        print(f"Quantity of Fittings: {quantity}")
+        print(f"Expected Pressure Loss: {expected_loss} kPa")
+        print(f"Calculated Pressure Loss: {result} kPa")
+        print(f"Absolute Tolerance: {tolerance}")
+        print(f"Result within Tolerance: {diff <= tolerance}")
+        print("-------------------------")
+
+test_pressure_loss_from_fittings()
+
+# Call the main function that is part of pytest so that the
+# computer will execute the test functions in this file.
+pytest.main(["-v", "--tb=line", "-rN", __file__])
+
+from pytest import approx
+import pytest
+from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe, pressure_loss_from_fittings, reynolds_number
+
+# Existing test functions...
+
+def test_reynolds_number():
+    test_cases = [
+        (0.048692, 0, 0, 1),
+        (0.048692, 1.65, 80069, 1),
+        (0.048692, 1.75, 84922, 1),
+        (0.28687, 1.65, 471729, 1),
+        (0.28687, 1.75, 500318, 1)
+    ]
+
+    tolerance = 1
+
+    for diameter, velocity, expected_reynolds, approx_tolerance in test_cases:
+        result = reynolds_number(diameter, velocity)
+        diff = abs(result - expected_reynolds)
+        print(f"Hydraulic Diameter: {diameter} meters")
+        print(f"Fluid Velocity: {velocity} m/s")
+        print(f"Expected Reynolds Number: {expected_reynolds}")
+        print(f"Calculated Reynolds Number: {result}")
+        print(f"Absolute Tolerance: {tolerance}")
+        print(f"Result within Tolerance: {diff <= tolerance}")
+        print("-------------------------")
+
+test_reynolds_number()
+
+# Call the main function that is part of pytest so that the
+# computer will execute the test functions in this file.
+pytest.main(["-v", "--tb=line", "-rN", __file__])
+
+from pytest import approx
+import pytest
+from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe, pressure_loss_from_fittings, reynolds_number, pressure_loss_from_pipe_reduction
+
+# Existing test functions...
+
+def test_pressure_loss_from_pipe_reduction():
+    test_cases = [
+        (0.28687, 0, 1, 0.048692, 0, 0.001),
+        (0.28687, 1.65, 471729, 0.048692, -163.744, 0.001),
+        (0.28687, 1.75, 500318, 0.048692, -184.182, 0.001)
+    ]
+
+    tolerance = 0.001
+
+    for larger_diameter, velocity, reynolds, smaller_diameter, expected_loss, approx_tolerance in test_cases:
+        result = pressure_loss_from_pipe_reduction(larger_diameter, velocity, reynolds, smaller_diameter)
+        diff = abs(result - expected_loss)
+        print(f"Larger Diameter: {larger_diameter} meters")
+        print(f"Fluid Velocity: {velocity} m/s")
+        print(f"Reynolds Number: {reynolds}")
+        print(f"Smaller Diameter: {smaller_diameter} meters")
+        print(f"Expected Pressure Loss: {expected_loss} kPa")
+        print(f"Calculated Pressure Loss: {result} kPa")
+        print(f"Absolute Tolerance: {tolerance}")
+        print(f"Result within Tolerance: {diff <= tolerance}")
+        print("-------------------------")
+
+test_pressure_loss_from_pipe_reduction()
+
+# Call the main function that is part of pytest so that the
+# computer will execute the test functions in this file.
+pytest.main(["-v", "--tb=line", "-rN", __file__])
